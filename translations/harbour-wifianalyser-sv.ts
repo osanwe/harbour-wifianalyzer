9042797220490ad73aad6f561e20e2e481289a8d
Åke Engelbrektson <eson57@users.noreply.github.com>
2015-07-14 13:19:33 +0200

    Create harbour-wifianalyser-sv.ts
    Swedish translation if wanted.

---
+++ translations/harbour-wifianalyser-sv.ts
@@ -0,0 +1,69 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>The simple tool for WiFi networks analyzing distributed under the terms of the GNU GPLv3.</source>
+        <translation>Ett enkelt verktyg för analys av WiFi-nätverk, distribuerat under villkoren för GNU GPLv3.</translation>
+    </message>
+    <message>
+        <source>This app uses icons by &lt;a href=&quot;http://www.flaticon.com/authors/rami-mcmin&quot;&gt;Rami McMin&lt;/a&gt; and &lt;a href=&quot;http://www.flaticon.com/authors/freepik&quot;&gt;Freepik&lt;/a&gt;.</source>
+        <translation>Denna app använder ikoner av &lt;a href=&quot;http://www.flaticon.com/authors/rami-mcmin&quot;&gt;Rami McMin&lt;/a&gt; och &lt;a href=&quot;http://www.flaticon.com/authors/freepik&quot;&gt;Freepik&lt;/a&gt;.</translation>
+    </message>
+    <message>
+        <source>You can connect with an author, improve the app or donate:</source>
+        <translation>Du kan kontakta en utvecklare, förbättra appen eller donera:</translation>
+    </message>
+    <message>
+        <source>&lt;a href=&quot;https://github.com/osanwe/harbour-wifianalyzer&quot;&gt;GitHub&lt;/a&gt;</source>
+        <translation>&lt;a href=&quot;https://github.com/osanwe/harbour-wifianalyzer&quot;&gt;GitHub&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;a href=&quot;https://twitter.com/Osanwe&quot;&gt;Ósanwe&lt;/a&gt;</source>
+        <translation>&lt;a href=&quot;https://twitter.com/Osanwe&quot;&gt;Ósanwe&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;a href=&quot;https://flattr.com/submit/auto?user_id=osanwe&amp;url=https://github.com/osanwe/harbour-wifianalyzer&amp;title=WiFi%20Analyzer&quot;&gt;Flattr&lt;/a&gt;</source>
+        <translation>&lt;a href=&quot;https://flattr.com/submit/auto?user_id=osanwe&amp;url=https://github.com/osanwe/harbour-wifianalyzer&amp;title=WiFi%20Analyzer&quot;&gt;Flattr&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <source>WiFi Analyzer v1.1.0</source>
+        <translation>WiFi Analyzer v1.1.0</translation>
+    </message>
+</context>
+<context>
+    <name>GraphPage</name>
+    <message>
+        <source>Set password</source>
+        <translation>Ange lösenord</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordPage</name>
+    <message>
+        <source>Save</source>
+        <translation>Spara</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <source>Type devel-su password:</source>
+        <translation>Ange devel-su-lösenord:</translation>
+    </message>
+    <message>
+        <source>Your devel-su password</source>
+        <translation>Ditt devel-su-lösenord</translation>
+    </message>
+    <message>
+        <source>Your password for devel-su will be saved only for this session. You have to type it after each applicaton starting. Your devel-su password is required for starting `wpa_cli` util.</source>
+        <translation>Ditt lösenord för &apos;devel-su&apos; sparas endast för denna session. Du måste ange det vid varje app-start. Ditt devel-su-lösenord krävs för att starta verktyget, wpa_cli.</translation>
+    </message>
+</context>
+</TS>
