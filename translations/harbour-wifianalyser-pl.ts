ddd036f7f2eb027e1d2f65cc8bdccdbe6ba1b316
mp107 <mp107@o2.pl>
2016-08-24 23:38:26 +0200

    Polish localization

---
+++ translations/harbour-wifianalyser-pl.ts
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="29"/>
+        <source>The simple tool for WiFi networks analyzing distributed under the terms of the GNU GPLv3.</source>
+        <translation>Proste narzędzie do analizowania sieci WiFi rozpowszechniane na warunkach GNU GPLv3.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="30"/>
+        <source>This app uses icons by</source>
+        <translation>Aplikacja używa ikon stworzonych przez</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="35"/>
+        <source>Localization</source>
+        <translation>Lokalizacja</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="36"/>
+        <source>Swedish</source>
+        <translation>Szwedzki</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="37"/>
+        <source>Czech</source>
+        <translation>Czeski</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="38"/>
+        <source>French</source>
+        <translation>Francuski</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="31"/>
+        <source>You can connect with an author, improve the app or donate:</source>
+        <translation>Możesz skontaktować się z autorem, ulepszyć aplikację lub przekazać darowiznę:</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="40"/>
+        <source>WiFi networks</source>
+        <translation>Sieci WiFi</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="53"/>
+        <source>Please, turn WiFi on</source>
+        <translation>Proszę włączyć WiFi</translation>
+    </message>
+</context>
+<context>
+    <name>GraphPage</name>
+    <message>
+        <location filename="../qml/pages/GraphPage.qml" line="204"/>
+        <source>Please, turn WiFi on</source>
+        <translation>Proszę włączyć WiFi</translation>
+    </message>
+</context>
+<context>
+    <name>ListPage</name>
+    <message>
+        <location filename="../qml/pages/ListPage.qml" line="34"/>
+        <source>Please, turn WiFi on</source>
+        <translation>Proszę włączyć WiFi</translation>
+    </message>
+</context>
+<context>
+    <name>TopMenu</name>
+    <message>
+        <location filename="../qml/views/TopMenu.qml" line="30"/>
+        <source>About</source>
+        <translation>O programie</translation>
+    </message>
+    <message>
+        <location filename="../qml/views/TopMenu.qml" line="35"/>
+        <source>Set as default</source>
+        <translation>Ustaw jako domyślne</translation>
+    </message>
+</context>
+</TS>
